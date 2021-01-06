388d88b8341901367ab808b6ef3ceecbd25c89fa
cheapskatecoder <cheapskatecoder@gmail.com>
2021-01-06 16:17:33 +0530

    Set debug to false

--- settings/prod.py
+++ settings/prod.py
@@ -3,7 +3,7 @@ import django_heroku
 import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["klantointerview.herokuapp.com"]
 
